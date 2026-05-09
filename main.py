7b7251c8f20ad4c6e9349af5d7fa49aae96b3649
Ikwuka CySec <242934426+IkwukaCySec@users.noreply.github.com>
2026-05-09 12:15:48 -0400

    Solve first to five steps of the challenge

--- main.py
+++ main.py
@@ -5,4 +5,20 @@ last_name ='Doe'
 print(first_name) # Outputs: John
 print(last_name) # Outputs: Doe
 
+# Step 2
+full_name = first_name + last_name
+print(full_name) # Outputs: JohnDoe
+
+# Step 3
+full_name = first_name + ' ' + last_name
+print(full_name) # Outputs: John Doe
+
+# Step 4
+address = '123 Main Street'
+print(address) # Outputs: 123 Main Stree
+
+# Step 5
+address += ', Apartment 4B'
+print(address) # Outputs: 123 Main Street, Apartment 4B
+
 
